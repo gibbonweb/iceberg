0aa107bdf8f4656b000a010db3ce01dc79dcfe18
Quinn Heagy <quinn@quinnheagy.com>
2012-03-13 19:06:43 -0400

    Added clean dates (customizable) and the assets folder is now copied over

--- _iceberg/Iceberg.php
+++ _iceberg/Iceberg.php
@@ -12,24 +12,31 @@ class Iceberg {
     
     public function generate($name = false) {
         if (!$name) die("Please enter the name of an article to compile.\n");
+
+        date_default_timezone_set($this->config["date"]["timezone"]);
         
-        $postPath = str_replace("(name)", $name, $this->config["posts"]["path"]);
+        $postFolder = str_replace("(name)", $name, $this->config["posts"]["path"]);
+        $postPath = str_replace("(name)", $name, $this->config["posts"]["file"]);
         $data = preg_split("/-----/", file_get_contents($postPath));
         
         $post = array();
         $post["info"] = Parser::yaml($data[1]);
-        $post["info"]["time"] = filemtime($postPath);
+        $post["info"]["time"] = date($this->config["date"]["format"], filemtime($postPath));
         $post["content"] = Parser::markdown($data[2]);
         
         $templatePath = str_replace("(template)", $post["info"]["layout"], $this->config["templates"]["path"]);
         $compiled = Template::toHTML($templatePath, $post);
         
         $outputPath = str_replace("(name)", $name, $this->config["posts"]["output"]);
-        mkdir($outputPath, 0700);
+        mkdir($outputPath, 0755);
         
+        Template::recursiveCopy($postFolder . "/assets", $outputPath . "/assets");
+
         $handle = fopen($outputPath."/index.html", "w");
         fwrite($handle, $compiled);
         fclose($handle);
+
+
         
         echo "$name successfully created at $outputPath\n";
 
--- _iceberg/Template.php
+++ _iceberg/Template.php
@@ -12,5 +12,23 @@ class Template {
         ob_end_clean();
         return $compiled;
     }
+
+    public static function recursiveCopy($src,$dst) { 
+	    $dir = opendir($src); 
+	    @mkdir($dst); 
+	    
+	    while(false !== ( $file = readdir($dir)) ) { 
+	        if (( $file != '.' ) && ( $file != '..' )) { 
+	            if ( is_dir($src . '/' . $file) ) { 
+	                recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+	            } 
+	            else { 
+	                copy($src . '/' . $file,$dst . '/' . $file); 
+	            } 
+	        } 
+	    } 
+
+	    closedir($dir); 
+	} 
     
 }
